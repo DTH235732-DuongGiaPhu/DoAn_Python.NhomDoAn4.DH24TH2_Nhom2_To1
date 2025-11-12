fb1b4a2cf3841ce6412c8f0160ee5ee6fd21bfc4
DuonGiPhu <duongphu010620@gmail.com>
2025-11-12 10:47:36 +0700

    Phú

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 import tkinter as tk
 from gui.login_window import LoginWindow
 from gui.main_menu import MainMenuWindow
-from database.book_database import getDbConnection
+from connection_manager import getDbConnection  # ← SỬA: Import đúng từ connection_manager
 
 if __name__ == '__main__':
     root = tk.Tk()
